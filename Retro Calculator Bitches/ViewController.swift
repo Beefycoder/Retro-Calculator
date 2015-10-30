9163a6017594fa1572590b9949035e7d00757fc4
Beefycode <pcbutler@shaw.ca>
2015-10-29 17:14:54 -0700

    Initial Commit

---
+++ Retro Calculator Bitches/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Retro Calculator Bitches
+//
+//  Created by Pat Butler on 2015-10-29.
+//  Copyright © 2015 RPG Ventures. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
